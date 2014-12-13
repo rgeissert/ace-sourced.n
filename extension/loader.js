40b874562d8ccf786f3f764e1a55bbb9a403bcdd
Raphael Geissert <atomo64@gmail.com>
2014-12-13 16:33:42 +0100

    Only load one script from within the extension Let the rest be loaded from the remote site, and cache that loader for one hour - this allows updates to the scripts to be independent from updates to the extension.

--- extension/loader.js
+++ extension/loader.js
@@ -1,26 +1,9 @@
 base = 'https://people.debian.org/~geissert/ace-debsources/';
 
-insertjs = document.createElement('script');
-insertjs.type = 'text/javascript';
-// needed, as ace won't know where to download its stuff from
-insertjs.attributes['data-ace-base'] = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/';
-insertjs.async = false;
-insertjs.src = insertjs.attributes['data-ace-base'] + 'ace.js';
-document.body.appendChild(insertjs);
-
-[
-    '/diff.js',
-    '/editor.js'
-].forEach(function(js) {
-	insertjs = document.createElement('script');
-	insertjs.type = 'text/javascript';
-	insertjs.src = base+js;
-	document.body.appendChild(insertjs);	
-    }
-);
+d = new Date();
+s = Date.UTC(d.getUTCFullYear(), d.getUTCDate(), d.getUTCDay());
 
 insertjs = document.createElement('script');
 insertjs.type = 'text/javascript';
-insertjs.async = false;
-insertjs.src = base+'/injector.js';
+insertjs.src = base+'/injector.js?'+s;
 document.body.appendChild(insertjs);
--- web/injector.js
+++ web/injector.js
@@ -1,3 +1,24 @@
+
+insertjs = document.createElement('script');
+insertjs.type = 'text/javascript';
+// needed, as ace won't know where to download its stuff from
+insertjs.attributes['data-ace-base'] = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/';
+insertjs.async = false;
+insertjs.src = insertjs.attributes['data-ace-base'] + 'ace.js';
+document.body.appendChild(insertjs);
+
+[
+    '/diff.js',
+    '/editor.js'
+].forEach(function(js) {
+	insertjs = document.createElement('script');
+	insertjs.type = 'text/javascript';
+	insertjs.async = false;
+	insertjs.src = js;
+	document.body.appendChild(insertjs);
+    }
+);
+
 window.haschange = undefined;
 
 function inject() {
