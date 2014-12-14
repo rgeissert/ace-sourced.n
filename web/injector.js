f5fd9dcf1955717152185acc48af60a31f0bab74
Raphael Geissert <atomo64@gmail.com>
2014-12-14 16:25:35 +0100

    Update to Ace 1.1.8, switch to jsdelivr as cdnjs doesn't have it

--- web/injector.js
+++ web/injector.js
@@ -3,7 +3,7 @@ base = 'https://people.debian.org/~geissert/ace-debsources/';
 insertjs = document.createElement('script');
 insertjs.type = 'text/javascript';
 // needed, as ace won't know where to download its stuff from
-insertjs.attributes['data-ace-base'] = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/';
+insertjs.attributes['data-ace-base'] = 'https://cdn.jsdelivr.net/ace/1.1.8/min/';
 insertjs.async = false;
 insertjs.src = insertjs.attributes['data-ace-base'] + 'ace.js';
 document.body.appendChild(insertjs);
