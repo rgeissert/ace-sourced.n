53a0ce5a6072fcfcd3d97594e9a7735b79894256
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-18 07:48:30 +0000

    add a firefox addon

---
+++ firefox/data/loader.js
@@ -0,0 +1,6 @@
+base = 'https://people.debian.org/~geissert/ace-debsources/';
+
+insertjs = document.createElement('script');
+insertjs.type = 'text/javascript';
+insertjs.src = base+'/injector.js';
+document.body.appendChild(insertjs);
---
+++ firefox/lib/main.js
@@ -0,0 +1,6 @@
+const data = require("sdk/self").data;
+const pageMod = require("sdk/page-mod");
+pageMod.PageMod({
+  include: "http://sources.debian.net/src/*",
+  contentScriptFile: data.url("loader.js")
+});
