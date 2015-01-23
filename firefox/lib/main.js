7592ee62c50c18b2f29bf9d67e4b93d67bb613a3
Raphael Geissert <atomo64@gmail.com>
2015-01-23 22:37:30 +0100

    Also load the editor when accessing sources.d.n via https

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,6 +1,9 @@
 const data = require("sdk/self").data;
 const pageMod = require("sdk/page-mod");
 pageMod.PageMod({
-  include: "http://sources.debian.net/src/*",
+  include: [
+    "http://sources.debian.net/src/*",
+    "https://sources.debian.net/src/*"
+  ],
   contentScriptFile: data.url("loader.js")
 });
